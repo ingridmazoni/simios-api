66614a28f3900ec46e6a2386fc9e32f38483d288
ingridmazoni <ingrid.mazoni01@fatec.sp.gov.br>
2020-10-28 00:57:23 -0300

    alterações no procfile

--- src/main/java/br/com/mercadolivre/simian/SimiosApplication.java
+++ src/main/java/br/com/mercadolivre/simian/SimiosApplication.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
@@ -59,6 +60,11 @@ public class SimiosApplication extends SpringBootServletInitializer {
 	}
 	
 	
+	  @Override
+	  protected SpringApplicationBuilder configure(
+	       SpringApplicationBuilder builder) {
+	         return builder.sources(SimiosApplication.class);
+	  }
 	
 	
 	
